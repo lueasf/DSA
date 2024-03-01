28e08801c39a26659591d1210c2593534459de83
CORREIA Lucie <lucie.correia@telecomnancy.eu>
2024-03-01 18:11:56 +0100

    dp

---
+++ 2_BinarySearch/lc74.py
@@ -0,0 +1,19 @@
+# mxn matrice , croissantes
+
+def searchMatrix(matrix, target):
+    liste = []
+    for i in range(len(matrix)): # i : 0,3
+        for j in range(len(matrix[0])): 
+            liste.append(matrix[i][j])
+    l,r = 0, len(liste)-1
+    while (l<=r):
+        m = (l + r) // 2
+        if liste[m] == target:
+            return True
+        if liste[m] > target:
+            r = m - 1
+        else:
+            l = m + 1
+    return False
+
+print(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
--- 4_ProgDyn/lc118.py
+++ 4_ProgDyn/lc118.py
@@ -18,3 +18,19 @@ class Solution:
                 output[j][k] = output[j-1][k-1] + output[j-1][k]
 
         return output
+    
+
+# PROG DYNAMIQUE
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ans = [[1]]
+
+        for i in range(1, numRows):
+            row = [1] * (i + 1)
+
+            for j in range(1, len(row) - 1):
+                row[j] = ans[i - 1][j - 1] + ans[i - 1][j]
+
+            ans.append(row)
+
+        return ans
